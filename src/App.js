c8e2b0f844a266920bc409e9dc0e90ddbd80ebce
matthew gregson-clark <mattieclark2020@outlook.com>
2022-09-18 20:06:07 +0100

    fist commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Beach from './components/Beach';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="App">
+      <Beach/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Beach.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+// import BeachImg from './logo.svg'
+
+function Beach(){
+    return (
+    <div className='beach_background'>
+        
+    </div>
+    );
+}
+
+
+export default Beach;

7e4a39e7190eba148af007698295708ada4b5a84
matthew gregson-clark <mattieclark2020@outlook.com>
2022-10-09 21:50:44 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 
 import './App.css';
-import Beach from './components/Beach';
+// import Beach from './components/Beach';
+import Arrow from './components/Arrow';
+import BeachName from './components/BeachName';
+import Narrow from './components/Narrow';
 
 function App() {
   return (
     <div className="container">
-      <div className="App">
-      <Beach/>
+      
+      <div className='beach_background'>
+
+        <div className="banner">
+            <div className="beach_name"><BeachName/></div>
+            <div className="NorthOrient"><Narrow/></div>
+        </div>
+
+            <div className="wind_arrow"><Arrow/></div>   
+            <div className="wave_arrow"><Arrow/></div>      
+             
+            
+        </div>
+     
       </div>
-    </div>
+      
+    
   );
 }
 

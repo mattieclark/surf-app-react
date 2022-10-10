4d945cdcf1041b2cf0600f9a390c4624d6b232c9
matthew gregson-clark <mattieclark2020@outlook.com>
2022-10-10 18:43:41 +0100

    basic components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
 
 import './App.css';
+// import react from 'react';
 // import Beach from './components/Beach';
 import Arrow from './components/Arrow';
 import BeachName from './components/BeachName';
 import Narrow from './components/Narrow';
+import Data from './components/Data';
+import SwellImage from './components/SwellImage';
 
 function App() {
   return (
     <div className="container">
-      
+
       <div className='beach_background'>
 
         <div className="banner">
-            <div className="beach_name"><BeachName/></div>
-            <div className="NorthOrient"><Narrow/></div>
+          <div className="beach_name"><BeachName /></div>
+          <div className="NorthOrient"><Narrow /></div>
         </div>
 
-            <div className="wind_arrow"><Arrow/></div>   
-            <div className="wave_arrow"><Arrow/></div>      
-             
-            
+        <div className='wind'>
+          <div className="wind_arrow"><Arrow /></div>
+          <div className="wind_data"><Data /></div>
         </div>
-     
+
+        <div className='wave'>
+          <div className="wave_arrow"><Arrow /></div>
+          <div className="wave_direction_data"><Data /></div>
+        </div>
+
+        <div className='swell'>
+        <div className="swell_data"><Data /></div>
+        <div className='swell_image'><SwellImage /></div>
+
+        </div>
+
+
+
+
       </div>
-      
-    
+
+    </div>
+
+
   );
 }
 
